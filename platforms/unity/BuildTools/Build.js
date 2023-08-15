1d70a1cd8d7ac02ea1820de759b1fa2345a0dd5d
Jordan Ranson <jordan@jordanranson.com>
2023-08-15 13:29:56 -0600

    build tools

--- platforms/unity/BuildTools/Build.js
+++ platforms/unity/BuildTools/Build.js
@@ -1,66 +1,79 @@
-const fs = require('fs-extra');
+const fs = require('fs-extra'); // You will need the 'fs-extra' package for recursive operations and more.
 const path = require('path');
-const glob = require('glob');
-const archiver = require('archiver');
+const glob = require('glob').glob;
+const archiver = require('archiver'); // This is used for creating zip files.
 
-async function main() {
-    const baseDir = path.resolve('.');
-    const buildDir = path.join(baseDir, 'Build');
-    const releaseDir = path.join(baseDir, 'Release');
-    const swagSDKPath = path.join(baseDir, 'Packages', 'SWAGSDK');
-    const packageJsonPath = path.join(swagSDKPath, 'package.json');
+async function runScript() {
+    const rootDir = '.';
+    const buildDir = './Build';
+    const packagesDir = './Packages/SWAGSDK';
+    const exampleProjectDir = './Build/Example Project';
+    const releaseDir = './Release';
+    const webGLTemplatesDir = './Build/WebGLTemplates';
 
-    // Delete './Build' folder
+    // Delete './Release'
+    await fs.remove(releaseDir);
+
+    // Delete './Build'
     await fs.remove(buildDir);
 
-    // Create './Build' folder
-    await fs.ensureDir(buildDir);
+    // Create './Build'
+    await fs.mkdir(buildDir);
 
-    // Copy './Packages/SWAGSDK' to './Build'
-    await fs.copy(swagSDKPath, path.join(buildDir, 'SWAGSDK'));
-
-    // Create './Build/Example Project'
-    await fs.ensureDir(path.join(buildDir, 'Example Project'));
-
-    // Load .gitignore
-    const gitignore = await fs.readFile(path.join(baseDir, '.gitignore'), 'utf-8');
-    const ignorePatterns = gitignore.split('\n').filter(line => line.trim() && !line.startsWith('#'));
-
-    // Copy and filter the contents of '.' to './Build/Example Project'
-    glob('**/*', {
-        cwd: baseDir,
-        ignore: ignorePatterns,
-        dot: true
-    }, (err, files) => {
-        if (err) throw err;
-        files.forEach(async file => {
-            const src = path.join(baseDir, file);
-            const dest = path.join(buildDir, 'Example Project', file);
-            await fs.copy(src, dest);
-        });
-    });
+    // Copy './Packages/SWAGSDK' into './Build'
+    await fs.copy(packagesDir, buildDir + '/SWAGSDK');
 
     // Copy './Packages/SWAGSDK/WebGLTemplates' to './Build'
-    await fs.copy(path.join(swagSDKPath, 'WebGLTemplates'), path.join(buildDir, 'WebGLTemplates'));
+    await fs.copy(path.join(packagesDir, 'WebGLTemplates'), webGLTemplatesDir);
 
-    // Create './Release' folder
-    await fs.ensureDir(releaseDir);
+    // Recursively delete all .meta files in './Build/WebGLTemplates'
+    const metaFiles = await glob(path.join(webGLTemplatesDir, '**/*.meta'));
+    for (const metaFile of metaFiles) {
+        await fs.remove(metaFile);
+    }
 
-    // Load package.json from './Packages/SWAGSDK' into a variable
-    const packageJson = await fs.readJson(packageJsonPath);
+    // Create './Build/Example Project'
+    await fs.mkdir(exampleProjectDir);
+
+    // Load .gitignore into memory
+    const gitignoreContent = await fs.readFile(path.join(rootDir, '.gitignore'), 'utf8');
+    const ignorePatterns = gitignoreContent.split('\n').filter(line => line.trim() !== '' && !line.startsWith('#'));
+    ignorePatterns.push('BuildTools');
+    ignorePatterns.push('package-lock.json');
+    ignorePatterns.push('package.json');
+
+    // Recursively copy '.' into './Build/Example Project', excluding .gitignore listed files
+    const exampleProjectfiles = await glob('*', {
+        cwd: rootDir,
+        ignore: ignorePatterns
+    });
+
+    for (const file of exampleProjectfiles) {
+        const src = path.join(rootDir, file);
+        const dest = path.join(exampleProjectDir, file);
+
+        await fs.copy(src, dest);
+    }
+
+    // Create './Release'
+    await fs.ensureDir(releaseDir); // This creates the folder if it doesn't exist.
+
+    // Load package.json into a variable
+    const packageJSON = await fs.readJson(path.join(packagesDir, 'package.json'));
 
     // Zip './Build' into './Release/SWAG Unity SDK (v{version} {unity}-{unityRelease}).zip'
-    const zipPath = path.join(releaseDir, `SWAG Unity SDK (v${packageJson.version} ${packageJson.unity}-${packageJson.unityRelease}).zip`);
-    const output = fs.createWriteStream(zipPath);
+    const outputZipPath = path.join(releaseDir, `SWAG Unity SDK (v${packageJSON.version} ${packageJSON.unity}-${packageJSON.unityRelease}).zip`);
+    const output = fs.createWriteStream(outputZipPath);
     const archive = archiver('zip', {
-        zlib: { level: 9 } // Compression level
+        zlib: { level: 9 } // Compression level.
     });
 
-    output.on('close', () => {
-        console.log(`Zipped to ${zipPath} successfully!`);
+    output.on('close', function() {
+        console.log(Math.round((archive.pointer() / 1024)).toLocaleString() + ' total kb');
+        console.log(`Release archive file created at ${outputZipPath}.`);
     });
 
-    archive.on('error', err => {
+    archive.on('error', function(err) {
         throw err;
     });
 
@@ -69,6 +82,6 @@ async function main() {
     archive.finalize();
 }
 
-main().catch(err => {
+runScript().catch(err => {
     console.error('An error occurred:', err);
 });
