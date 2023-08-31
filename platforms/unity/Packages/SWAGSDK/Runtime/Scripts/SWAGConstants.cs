5088c0f9e86403c17c0d0718ae504fe93ef2f9d2
Jordan Ranson <jordan@jordanranson.com>
2023-08-31 10:06:59 -0600

    SWAGAPI-106 restoring swag services urls

--- platforms/unity/Packages/SWAGSDK/Runtime/Scripts/SWAGConstants.cs
+++ platforms/unity/Packages/SWAGSDK/Runtime/Scripts/SWAGConstants.cs
@@ -6,8 +6,7 @@ namespace AddictingGames
     {
         /* #region Addicting Games */
 
-        // public static string AddictingGamesServicesURL = "https://swag-services.shockwave.com/v1";
-        public static string AddictingGamesServicesURL = "http://local.addictinggames.com:8080/v1";
+        public static string AddictingGamesServicesURL = "https://swag-services.addictinggames.com/v1";
         public static string AddictingGamesPreloaderURL = "https://cdn2.addictinggames.com/addictinggames-content/ag-assets/preloaders/AG_Preloader_2023.webm";
         public static string AddictingGamesPrimaryColor = "#E33225";
 
@@ -17,8 +16,7 @@ namespace AddictingGames
 
         /* #region Shockwave */
 
-        // public static string ShockwaveServicesURL = "https://swag-services.shockwave.com/v1";
-        public static string ShockwaveServicesURL = "http://local.shockwave.com:8080/v1";
+        public static string ShockwaveServicesURL = "https://swag-services.shockwave.com/v1";
         public static string ShockwavePreloaderURL = "https://cdn2.addictinggames.com/addictinggames-content/ag-assets/preloaders/AG_Preloader_2023.webm";
         public static string ShockwavePrimaryColor = "#195AAB";
 
