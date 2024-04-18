f431f7cb21b8a8d05eb3381411f72b51a8883b67
rhysgjones <>
2024-04-18 09:31:54 -0600

    Move stub methods to separate file

--- platforms/html5/src/api/api.js
+++ platforms/html5/src/api/api.js
@@ -7,6 +7,7 @@ var session = require('../session');
 var data = require('./data');
 var ui = require('./ui');
 var utils = require('../utils');
+var stubMethods = require("./stub.js")
 
 var _isRendering = false;
 // -----------------------------------------------------------------------------
@@ -54,6 +55,7 @@ function SWAGAPI(options) {
 };
 
 var activeMethods = {
+  
   startSession: function() {
     var self = this;
     return data.getEntity()
@@ -166,119 +168,6 @@ var activeMethods = {
 
   getCurrentUser: function() {
     return data.getCurrentUser();
-  },
-
-  userLogout:  function() {
-    return data.userLogout();
-  }
-};
-
-var stubMethods = {
-  startSession: function() {
-    var self = this;
-    utils.debug('session ready');
-    self.emit(config.events.SESSION_READY, { session_ready: true });
-    return Promise.resolve();
-  },
-
-  getScoreCategories: function() {
-    return Promise.resolve();
-  },
-
-  getDays: function(limit) {
-    return Promise.resolve();
-  },
-
-  getScores: function(options) {
-    return Promise.resolve();
-  },
-
-  postScore: function(level_key, value, options) {
-    return Promise.resolve();
-  },
-
-  postDailyScore: function(day, level_key, value) {
-    return Promise.resolve();
-  },
-
-  getAchievementCategories: function() {
-    return Promise.resolve();
-  },
-
-  postAchievement: function(achievement_key) {
-    return Promise.resolve();
-  },
-
-  getUserAchievements: function() {
-    return Promise.resolve();
-  },
-
-  postDatastore: function(key, value) {
-    return Promise.resolve();
-  },
-
-  getUserDatastore: function() {
-    return Promise.resolve();
-  },
-
-  populateLevelSelect: function(domId) {
-    return Promise.resolve();
-  },
-
-  populateDaySelect: function(domId, limit) {
-    return Promise.resolve();
-  },
-
-  populateAchievementSelect: function(domId) {
-    return Promise.resolve();
-  },
-
-  getCurrentEntity: function() {
-    return {};
-  },
-
-  showDialog: function(type, options) {
-    return Promise.resolve();
-  },
-
-  isSubscriber: function() {
-    return false;
-  },
-
-  hasDailyScore: function(level_key) {
-    return false;
-  },
-
-  getCurrentDay: function() {
-    return data.getCurrentDay();
-  },
-
-  getBrandingLogo: function() {
-    return ui.getBrandingLogo();
-  },
-
-  getBrandingLogoUrl: function() {
-    return ui.getBrandingLogoUrl();
-  },
-
-  startGame: function() {
-    return ui.startGame();
-  },
-
-  endGame: function() {
-    return ui.endGame();
-  },
-
-  showAd: function() {
-    return ui.showAd();
-  },
-
-  postExternalMessage: function(message) {
-    return data.postExternalMessage(message);
-  },
-
-  getCurrentUser: function() {
-    return Promise.resolve({});
   },
 
   userLogout:  function() {
