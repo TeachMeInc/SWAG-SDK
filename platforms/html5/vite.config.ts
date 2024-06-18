219a0b0d856b6605ba042f0d0754e71ff8f40548
Jordan Ranson <jranson@shockwave.com>
2024-06-17 22:19:28 -0600

    SWQA-1629 build

--- platforms/html5/vite.config.ts
+++ platforms/html5/vite.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// @ts-nocheck
+
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +10,21 @@ export default defineConfig({
   assetsInclude: ["**/*.handlebars"],
   server: {
     port: 8888
-  }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'SWAGAPI',
+      fileName: 'swag-api',
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') return 'swag-api.css';
+          return assetInfo.name;
+        },
+      },
+    },
+  },
 })
