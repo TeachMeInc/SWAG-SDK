b7cd744b8e652d3213f9b7866f9f6b823eea8790
Jordan Ranson <jranson@shockwave.com>
2025-10-16 15:29:47 -0600

    bump version

--- platforms/html5/types/swag-api.d.ts
+++ platforms/html5/types/swag-api.d.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-// Type definitions for SWAG HTML5 SDK v5.4.4
+// Type definitions for SWAG HTML5 SDK v5.4.5
 // Visit https://developers.shockwave.com for more information
 
 interface Entity {
